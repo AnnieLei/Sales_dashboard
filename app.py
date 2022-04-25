68a8905f8a9fb794ae1350e857870ff5ad4d8be0
Lei <55474074+AnnieLei@users.noreply.github.com>
2022-04-25 17:06:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -162,14 +162,4 @@ st.markdown('<p class="font">Wonder what the data looks like?</p>', unsafe_allow
 
 st.dataframe(df_selection)
 
-def convert_df(df):
-     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-     return df.to_csv().encode('utf-8')
 
-csv = convert_df(my_large_df)
-st.download_button(
-     label="Download data as CSV",
-     data=csv,
-     file_name='salesx.csv',
-     mime='text/csv',
- )
